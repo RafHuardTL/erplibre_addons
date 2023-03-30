615b8bf63c7ad40f14c554dd3fd38a37894811f6
RafHuardTL <rafael.huard@technolibre.ca>
2023-03-30 15:53:49 -0400

    ADD aliments: fonctionnalité d'ajout d'aliments via AJAX

--- rhuard_snippet/controllers/main.py
+++ rhuard_snippet/controllers/main.py
@@ -1,7 +1,40 @@
 from odoo import http
+import uuid
 
 
 class RafaelHuardSnippetController(http.Controller):
+    aliment_list = dict()
+    aliment_list_hardcoded = dict()
+
+    def __init__(self):
+        self.aliment_list_hardcoded = {
+            "aliments": [
+                {
+                    "id": 1,
+                    "name": "Carotte"
+                },
+                {
+                    "id": 2,
+                    "name": "Lait"
+                },
+                {
+                    "id": 3,
+                    "name": "Chocolat"
+                },
+                {
+                    "id": 4,
+                    "name": "Orange"
+                },
+                {
+                    "id": 5,
+                    "name": "Pain"
+                }
+            ]
+        }
+        self.aliment_list = {
+            "aliments": []
+        }
+
     @http.route(
         ["/rhuard_snippet/bonjour"],
         type="json",
@@ -14,30 +47,50 @@ class RafaelHuardSnippetController(http.Controller):
         return {"message": "Bonjour, le monde!"}
 
     @http.route(
+        ["/rhuard_snippet/aliments_hardcoded"],
+        type="json",
+        auth="public",
+        website=True,
+        methods=["POST", "GET"],
+        csrf=False,
+    )
+    def aliments_hardcoded(self):
+        return self.aliment_list_hardcoded
+
+    @http.route(
         ["/rhuard_snippet/aliments"],
         type="json",
         auth="public",
         website=True,
         methods=["POST", "GET"],
-        csrf=False
+        csrf=False,
     )
     def aliments(self):
-        return {
-            "aliments": [
-                {
-                    "name": "Carotte"
-                },
-                {
-                    "name": "Lait"
-                },
-                {
-                    "name": "Chocolat"
-                },
-                {
-                    "name": "Orange"
-                },
-                {
-                    "name": "Pain"
-                }
-            ]
+        return self.aliment_list
+
+    @http.route(
+        ["/rhuard_snippet/add_aliment"],
+        type="json",
+        auth="public",
+        website=True,
+        methods=["POST"],
+        csrf=False,
+    )
+    def add_aliment(self, **kwargs):
+        if not kwargs.get("aliment_name"):
+            return False
+
+        aliments_with_same_name = tuple((
+            aliment for aliment in self.aliment_list.get("aliments")
+            if aliment["name"] == kwargs.get("aliment_name")
+        ))
+
+        if len(aliments_with_same_name) > 0:
+            return False
+
+        new_aliment = {
+            "id": uuid.uuid4(),
+            "name": kwargs.get("aliment_name")
         }
+        self.aliment_list.get("aliments").append(new_aliment)
+        return new_aliment
--- rhuard_snippet/static/src/js/website.rhuard_snippet.animation.js
+++ rhuard_snippet/static/src/js/website.rhuard_snippet.animation.js
@@ -2,6 +2,7 @@ odoo.define("rhuard_snippet.animation", function (require) {
     "use strict";
 
     let sAnimation = require("website.content.snippets.animation");
+    let ajax = require("web.ajax");
 
     sAnimation.registry.rhuard_snippet = sAnimation.Class.extend({
         selector: ".o_rhuard_snippet",
@@ -10,16 +11,28 @@ odoo.define("rhuard_snippet.animation", function (require) {
             let self = this;
             let _super = self._super.bind(this);
 
+            // Objectif 1
             this._eventListText = this.$(".rh-text__value");
-            this._eventListAliments = this.$(".rh-aliment__list");
+            // Objectif 2
+            this._eventListAlimentsHardcoded = this.$(".rh-aliment__list");
+            // Objectif 3
+            this._sendPostAlimentForm = this.$(".rh-add-aliment__form");
+            this._sendPostAlimentInput = this.$(".rh-add-aliment__input");
+            this._eventListAliments = this.$(".rh-add-aliment__list");
 
             this._originalContentText = this._eventListText.text();
+            this._originalContentAlimentsHardcoded = this._eventListAlimentsHardcoded.html();
             this._originalContentAliments = this._eventListAliments.html();
 
-            const def1 = this.bonjour(self, this._eventListText);
-            const def2 = this.aliments(self, this._eventListAliments);
+            $(this._sendPostAlimentInput).val("");
 
-            return $.when(def1, def2).done(function () {
+            const def1 = this.bonjour(self, this._eventListText);
+            const def2 = this.updateAliments(self, this._eventListAlimentsHardcoded, "/rhuard_snippet/aliments_hardcoded");
+            const def3 = this.updateAliments(self, this._eventListAliments, "/rhuard_snippet/aliments");
+
+            this.add_aliment(self, this._sendPostAlimentInput);
+
+            return $.when(def1, def2, def3).done(function () {
                 _super.apply(this, arguments);
             });
         },
@@ -41,9 +54,9 @@ odoo.define("rhuard_snippet.animation", function (require) {
 
             return def;
         },
-        aliments: function (self, eventList) {
+        updateAliments: function (self, eventList, route) {
             let def = self._rpc({
-                route: "/rhuard_snippet/aliments",
+                route: route,
             }).then(function (data) {
                 if (data.error) {
                     return;
@@ -59,19 +72,44 @@ odoo.define("rhuard_snippet.animation", function (require) {
                 $(eventList).html("");
                 for (const aliment of aliments) {
                     const el = document.createElement("li");
+                    el.id = aliment.id;
                     el.textContent = aliment.name;
                     el.className = "rhuard_aliment";
                     eventList.append(el);
                 }
+
+                if (!$(eventList).html()) {
+                    $(eventList).parent().hide();
+                } else {
+                    $(eventList).parent().show();
+                }
             });
 
             return def;
+        },
+        add_aliment: function (self, eventList) {
+            $(self._sendPostAlimentForm).submit(function (event) {
+                event.preventDefault();
+
+                if (!$(eventList).val()) {
+                    return;
+                }
+
+                ajax.jsonRpc(
+                    "/rhuard_snippet/add_aliment",
+                    "call",
+                    {"aliment_name": $(eventList).val()}
+                ).done(function (data) {
+                    self.updateAliments(self, self._eventListAliments, "/rhuard_snippet/aliments")
+                });
+            })
         },
         destroy: function () {
             this._super.apply(this, arguments);
             if (this._$loadedContentText) {
                 this._eventListText.text(this._originalContentText);
             }
+            this._eventListAlimentsHardcoded.html(this._originalContentAlimentsHardcoded);
             this._eventListAliments.html(this._originalContentAliments);
         }
     });
