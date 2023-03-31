44c4485d383118acd7080afb2f635b61407d022e
RafHuardTL <rafael.huard@technolibre.ca>
2023-03-31 10:05:25 -0400

    UPD aliments: renommage des variables JS des éléments HTML.

--- rhuard_snippet/static/src/js/website.rhuard_snippet.animation.js
+++ rhuard_snippet/static/src/js/website.rhuard_snippet.animation.js
@@ -12,25 +12,26 @@ odoo.define("rhuard_snippet.animation", function (require) {
             let _super = self._super.bind(this);
 
             // Objectif 1
-            this._eventListText = this.$(".rh-text__value");
+            this._obj1Text = this.$(".rh-text__value");
             // Objectif 2
-            this._eventListAlimentsHardcoded = this.$(".rh-aliment__list");
+            this._obj2ListAliments = this.$(".rh-aliment__list");
             // Objectif 3
-            this._sendPostAlimentForm = this.$(".rh-add-aliment__form");
-            this._sendPostAlimentInput = this.$(".rh-add-aliment__input");
-            this._eventListAliments = this.$(".rh-add-aliment__list");
+            this.__obj3Form = this.$(".rh-add-aliment__form");
+            this._obj3Input = this.$(".rh-add-aliment__input");
+            this._obj3ListAliments = this.$(".rh-add-aliment__list");
+            // Objectif 4
 
-            this._originalContentText = this._eventListText.text();
-            this._originalContentAlimentsHardcoded = this._eventListAlimentsHardcoded.html();
-            this._originalContentAliments = this._eventListAliments.html();
+            this._obj1OriginalContentText = this._obj1Text.text();
+            this._obj2OriginalContentAliments = this._obj2ListAliments.html();
+            this._obj3OriginalContentAliments = this._obj3ListAliments.html();
 
-            $(this._sendPostAlimentInput).val("");
+            $(this._obj3Input).val("");
 
-            const def1 = this.bonjour(self, this._eventListText);
-            const def2 = this.updateAliments(self, this._eventListAlimentsHardcoded, "/rhuard_snippet/aliments_hardcoded");
-            const def3 = this.updateAliments(self, this._eventListAliments, "/rhuard_snippet/aliments");
+            const def1 = this.bonjour(self, this._obj1Text);
+            const def2 = this.updateAliments(self, this._obj2ListAliments, "/rhuard_snippet/aliments_hardcoded");
+            const def3 = this.updateAliments(self, this._obj3ListAliments, "/rhuard_snippet/aliments");
 
-            this.add_aliment(self, this._sendPostAlimentInput);
+            this.add_aliment(self, this._obj3Input);
 
             return $.when(def1, def2, def3).done(function () {
                 _super.apply(this, arguments);
@@ -88,7 +89,7 @@ odoo.define("rhuard_snippet.animation", function (require) {
             return def;
         },
         add_aliment: function (self, eventList) {
-            $(self._sendPostAlimentForm).submit(function (event) {
+            $(self._obj3Form).submit(function (event) {
                 event.preventDefault();
 
                 if (!$(eventList).val()) {
@@ -107,10 +108,10 @@ odoo.define("rhuard_snippet.animation", function (require) {
         destroy: function () {
             this._super.apply(this, arguments);
             if (this._$loadedContentText) {
-                this._eventListText.text(this._originalContentText);
+                this._obj1Text.text(this._obj1OriginalContentText);
             }
-            this._eventListAlimentsHardcoded.html(this._originalContentAlimentsHardcoded);
-            this._eventListAliments.html(this._originalContentAliments);
+            this._obj2ListAliments.html(this._obj2OriginalContentAliments);
+            this._obj3ListAliments.html(this._obj3OriginalContentAliments);
         }
     });
 });
