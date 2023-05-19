3a0101a7f8a4e10b797e77342b31baf3ff60fcf7
RafHuardTL <rafael.huard@technolibre.ca>
2023-05-19 19:14:03 -0400

    [ADD] Ajout du hook de création d'aliment pour le long polling.

--- rest/models/aliment.py
+++ rest/models/aliment.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from datetime import datetime
+from odoo import api, models, fields
 
 
 class AlimentModel(models.Model):
@@ -12,3 +13,24 @@ class AlimentModel(models.Model):
     int = fields.Integer(string="aliment_int")
     float = fields.Float(string="aliment_float")
     bool = fields.Boolean(string="aliment_bool")
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        for rec in res:
+            self.env["bus.bus"].sendone(
+                # f'["{self._cr.dbname}","{self._name}",{rec.id}]',
+                # TODO choose unique canal name for the member
+                "rest.aliment",
+                {
+                    "timestamp": str(datetime.now()),
+                    "data": {
+                        "name": rec.name,
+                        "id": rec.id
+                    },
+                    "field_id": rec.id,
+                    "canal": f'["{self._cr.dbname}","{self._name}"]',
+                    "action": "add"
+                },
+            )
+        return res
